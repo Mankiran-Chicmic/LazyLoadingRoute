54dbbe0652b670d59bc665124c37d2b879350662
mankiran8910 <94637111+mankiran8910@users.noreply.github.com>
2023-03-06 13:37:52 +0530

    chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { ListComponent } from './admin/list/list.component';
+//import { LoginComponent } from './admin/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'admin',loadChildren:()=>import('./admin/admin.module').
+ then(mod=>mod.AdminModule)},
+ {path:'user',loadChildren:()=>import('./user/user.module').
+ then(mod=>mod.UserModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { LoginComponent } from './admin/login/login.component';
+//import { ListComponent } from './admin/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
